d1b121c3253b901f163fc6ada7dce4fe6cfd6755
chriswei <chriswei@c-67-180-144-141.hsd1.ca.comcast.net>
2009-07-20 19:02:49 -0700

    Adding the spriteConfig.py file for the current set of props, which I'd missed the first time around.

---
+++ data/sprites/terrain/jungle/grass/props/trees/spriteConfig.py
@@ -0,0 +1,15 @@
+standardPolygon = [(0, 0), (10, 0), (5, 10)]
+sprites = {
+    '1' : {
+        'polygon' : standardPolygon
+    },
+    '2' : {
+        'polygon' : standardPolygon
+    },
+    '3' : {
+        'polygon' : standardPolygon
+    },
+    '4' : {
+        'polygon' : standardPolygon
+    },
+}
