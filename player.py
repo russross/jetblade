d3ca1b107b647a804ab7fd69b999f9b9d4607eff
chriswei <chriswei@c-67-180-144-141.hsd1.ca.comcast.net>
2009-08-11 12:44:10 -0700

    Added reactions to being hit by an enemy to the Player. This includes health (with a simple health display), flinching when hit, mercy invincibility, and deallocation when health runs out.

--- player.py
+++ player.py
@@ -1,7 +1,21 @@
 import terrestrialobject
 import game
 import logger
+import font
+import constants
 from vector2d import Vector2D
+
+## Number of frames of invincibility to get after getting hit
+mercyInvincibilityFrames = 45
+## Default starting health
+defaultHealth = 100
+## Velocity to impart when hit (when facing right)
+flinchVel = Vector2D(-40, -10)
+## Number of frames to flinch for
+flinchFrames = 3
+
+energyDisplayLoc = Vector2D(20, 20)
+energyDisplayColor = (250, 250, 100, 150)
 
 ## The Player class handles the player's avatar in the game. 
 class Player(terrestrialobject.TerrestrialObject):
@@ -10,10 +24,19 @@ class Player(terrestrialobject.TerrestrialObject):
     def __init__(self):
         terrestrialobject.TerrestrialObject.__init__(self, game.map.getStartLoc().toRealspace(), 'maleplayer')
         self.canHang = True
+        self.faction = 'player'
+        ## Amount of health remaining before dying
+        self.health = defaultHealth
+        ## Invincibility time after getting hit
+        self.invincibilityTimer = 0
 
 
     ## Set appropriate flags according to the current input.
     def AIUpdate(self):
+        if mercyInvincibilityFrames - self.invincibilityTimer < flinchFrames:
+            # No updates to state when flinching.
+            self.invincibilityTimer -= 1
+            return
         shouldCrawl = False 
         isJumping = False 
         runDirection = 0
@@ -61,4 +84,42 @@ class Player(terrestrialobject.TerrestrialObject):
             self.isClimbing = True
 
         self.runDirection = runDirection
+
+        if self.invincibilityTimer > 0:
+            self.invincibilityTimer -= 1
+            if (self.sprite.getCurrentAnimation() == 'flinch' and 
+                    mercyInvincibilityFrames - self.invincibilityTimer >= flinchFrames):
+                # Restore control after finishing the flinch.
+                self.isAnimationLocked = False
+                self.isGravityOn = True
+                self.shouldApplyVelocityCap = True
+
+
+    ## Make the player flicker if they're invincible. Display the player's
+    # vital statistics.
+    def draw(self, screen, *args):
+        if self.invincibilityTimer == 0 or game.frameNum % 2 == 1:
+            terrestrialobject.TerrestrialObject.draw(self, screen, *args)
+        # \todo This draws on the same layer the player does (so e.g. behind
+        # terrain). 
+        game.fontManager.drawText(
+                    'MODENINE', screen, 
+                    ["Energy: %d" % self.health], 
+                    energyDisplayLoc, 18, font.TEXT_ALIGN_LEFT,
+                    energyDisplayColor)
+
+
+    ## Process a collision. Take damage from enemies if we're vulnerable
+    def processCollision(self, collision):
+        terrestrialobject.TerrestrialObject.processCollision(self, collision)
+        if collision.type == 'enemy' and self.invincibilityTimer == 0:
+            self.invincibilityTimer = mercyInvincibilityFrames
+            self.health -= collision.altObject.touchDamage
+            self.sprite.setAnimation('flinch')
+            self.vel = Vector2D(flinchVel.x * cmp(collision.altObject.loc.x, self.loc.x), flinchVel.y)
+            self.isGrounded = False
+            self.isAnimationLocked = True
+            self.isGravityOn = False
+            self.shouldApplyVelocityCap = False
+
 
