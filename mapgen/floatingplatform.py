34b8b2a677e7bd5b767f256263165a6358adf6c4
chriswei <chriswei@c-76-21-45-96.hsd1.ca.comcast.net>
2011-03-16 20:09:03 -0700

    Minor comments tweak; no code changes.

--- mapgen/floatingplatform.py
+++ mapgen/floatingplatform.py
@@ -6,15 +6,16 @@ import pygame
 # mapgen.
 class FloatingPlatform:
     ## Instantiate a FloatingPlatform. 
-    # The loc and width parameters are in gridspace units; convert them into
-    # realspace units because the QuadTree class works better on larger units.
+    # The loc and width parameters are in gridspace units.
     # \param loc The location, in gridspace, of the platform
     # \param width The width, in blocks, of the platform.
     def __init__(self, loc, width):
         self.loc = loc
         self.width = width
 
-    ## Return a bounding box for the platform.
+    ## Return a bounding box for the platform. We use realspace units here 
+    # because the quadtree we're storing these objects in works better with
+    # larger numbers (it has a fixed minimum grid size).
     def getBounds(self):
         loc = self.loc.toRealspace()
         rect = pygame.Rect(loc.x, loc.y, self.width * constants.blockSize, constants.blockSize)
